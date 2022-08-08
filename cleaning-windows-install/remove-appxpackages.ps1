75540fcb1f0248d91a5a45975f5a88f10fa551a1
Austin George <austin.george@fsgi.com>
2022-08-08 15:47:16 -0500

    Added .ps1 file

---
+++ cleaning-windows-install/remove-appxpackages.ps1
@@ -0,0 +1,6 @@
+﻿Get-AppXPackage *Amazon* -allUsers | Remove-AppXPackage -allUsers
+Get-AppXPackage *Booking* -allUsers | Remove-AppXPackage -allUsers
+Get-AppXPackage *Disney* -allUsers | Remove-AppXPackage -allUsers
+Get-AppXPackage *Dolby* -allUsers | Remove-AppXPackage -allUsers
+Get-AppXPackage *Facebook* -allUsers | Remove-AppXPackage -allUsers
+Get-AppXPackage *SAMSUNGELECTRONICSC* -allUsers | Remove-AppXPackage -allUsers
